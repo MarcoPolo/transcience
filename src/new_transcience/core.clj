09ce3485b45980f622d2182e7581ee3c88c66d72
Marco Munizaga <marco.munizaga@grooveshark.com>
2013-04-18 02:08:50 -0400

    Levels are now saved with hashes

--- src/new_transcience/core.clj
+++ src/new_transcience/core.clj
@@ -1,19 +1,43 @@
 (ns new-transcience.core
   (:use compojure.core)
+  (:import [java.security MessageDigest])
   (:require [compojure.handler :as handler]
             [compojure.route :as route]))
+
+(def directory-name "resources/levels")
+(def directory (clojure.java.io/file directory-name))
+
+(defn hash-str [input] 
+  (let [hash (MessageDigest/getInstance "SHA-256")]
+    (. hash update (.getBytes input))
+    (let [digest (.digest hash)]
+      (apply str (map #(format "%02x" (bit-and % 0xff)) digest)))))
+
+(defn hash-exists? [hash]
+  (seq (filter #(= hash (.getName %)) (file-seq directory))))
+
+(defn save-level! [things]
+  (let [hash (hash-str (str (rand)))]
+    (if (hash-exists? hash)
+      (save-level! things)
+      (do 
+        (spit (str directory-name "/" hash) things)
+        hash))))
+
+(defn valid-hash? [hash]
+  (try (Long/parseLong "AFF" 16) (catch Exception e false)))
+
 
 (defroutes app-routes
 
            (route/files "/" {:root "www"})
            (POST "/saveThings" [things level]
-                 (spit (str "resources/things" "-" (Integer. level)) things)
-                 "nice"
-                 )
+                 (save-level! things))
            (GET "/things" [level]
-                (println "getting data for " level)
-                (slurp (str "resources/things-" (Integer. level))))
-
+                (if (valid-hash? level)
+                  (slurp (str directory-name "/" level))
+                  {:error "Wrong Hash"}))
            (route/not-found "<h1>Page not found</h1>"))
 (def app
   (handler/site app-routes))
+
